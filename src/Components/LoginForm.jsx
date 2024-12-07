8ab44d7e0161b854d7d67f2cd3019dfea363f2d7
JonathanMontalvo <jmp20245@gmail.com>
2024-12-07 15:18:30 -0600

    Mejora del diseño del login y de registrar, ademas se añadio el rol de Administrador

--- src/Components/LoginForm.jsx
+++ src/Components/LoginForm.jsx
@@ -1,129 +1,187 @@
-import { useState } from "react";
-import { Button, Text, StyleSheet, TextInput, View } from "react-native";
+import { useState } from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
 
-import {app,db} from '../utils/firebase.js'
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
-import { getFirestore, collection, query, where, getDocs } from "firebase/firestore";
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { app, db } from '../utils/firebase.js';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import {
+  getFirestore,
+  collection,
+  query,
+  where,
+  getDocs,
+} from 'firebase/firestore';
 
-export default function LoginForm(
-    {
-        changeForm = () => { },setRol,setUsuario
+export default function LoginForm({
+  changeForm = () => {},
+  setRol,
+  setUsuario,
+}) {
+  const [data, setData] = useState({ email: '', password: '' });
+
+  const updateData = (newValue, campo) => {
+    const newData = { ...data };
+    newData[campo] = newValue;
+    setData(newData);
+  };
+
+  const singin = async () => {
+    const { email, password } = data;
+    try {
+      const auth = getAuth(app);
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+
+      const user = userCredential.user; // El usuario autenticado
+      const usersCollection = collection(db, 'usuarios');
+      const q = query(usersCollection, where('Correo', '==', email));
+
+      const querySnapshot = await getDocs(q); // Ahora utilizamos await aquí
+
+      if (!querySnapshot.empty) {
+        const userDoc = querySnapshot.docs[0];
+        const rol = userDoc.data().rol; // Suponiendo que tienes un campo "rol"
+        setUsuario({
+          Nombre: userDoc.data().Nombre,
+          ApellidoP: userDoc.data().Apellido_Paterno,
+          ApellidoM: userDoc.data().Apellido_Materno,
+          id: userDoc.id,
+        });
+        setRol(rol);
+        return rol;
+      } else {
+        alert('No se Encontraron las Crendenciales');
+        console.log('No se encontró el usuario con el correo proporcionado');
+      }
+    } catch (error) {
+      if (error.code === 'auth/invalid-email') {
+        alert(
+          'El correo electrónico no es válido. Por favor ingresa un correo electrónico válido.'
+        );
+      }
+      if (error.code === 'auth/missing-password') {
+        alert('Favor de colocar la contraseña');
+      }
+      if (error.code === 'auth/invalid-credential') {
+        alert('Credenciales no Validas');
+      }
+      console.log('Error:', error.message);
     }
-) {
+  };
 
-    const [data, setData] = useState({ email: '', password: '' })
+  const [showPassword, setShowPassword] = useState(false);
 
-    const updateData = (newValue, campo) => {
-        const newData = { ...data }
-        newData[campo] = newValue
-        setData(newData)
-    }
+  console.log(data);
 
-    const singin = async () => {
-        const { email, password } = data;
-        try {
-            const auth = getAuth(app);
-            const userCredential = await signInWithEmailAndPassword(auth, email, password); 
-    
-            const user = userCredential.user;  // El usuario autenticado
-            const usersCollection = collection(db, "usuarios");
-            const q = query(usersCollection, where("Correo", "==", email));
-            
-            const querySnapshot = await getDocs(q); // Ahora utilizamos await aquí
-    
-            if (!querySnapshot.empty) {
-                const userDoc = querySnapshot.docs[0];
-                const rol = userDoc.data().rol;  // Suponiendo que tienes un campo "rol"
-                setUsuario({"Nombre":userDoc.data().Nombre,
-                            "ApellidoP":userDoc.data().Apellido_Paterno,
-                            "ApellidoM":userDoc.data().Apellido_Materno,
-                            "id":userDoc.id
-                        })
-                setRol(rol)
-                return rol;
-            } else {
-                alert("No se Encontraron las Crendenciales")
-                console.log("No se encontró el usuario con el correo proporcionado");
-            }
-        } catch (error) {
-        if (error.code === 'auth/invalid-email') {
-            alert('El correo electrónico no es válido. Por favor ingresa un correo electrónico válido.');
-        } 
-        if (error.code === 'auth/missing-password') {
-            alert('Favor de colocar la contraseña');
-        } 
-        if (error.code === 'auth/invalid-credential') {
-            alert('Credenciales no Validas');
-        } 
-            console.log('Error:', error.message);
-        }
-    }
-    
-    console.log(data)
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>LOGIN</Text>
+      <TextInput
+        style={[styles.input]}
+        placeholder='Ingresa tu correo'
+        placeholderTextColor='#fff'
+        onChangeText={(nv) => {
+          updateData(nv, 'email');
+        }}
+      />
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.inputField}
+          placeholder='Ingresa tu contraseña'
+          placeholderTextColor='#fff'
+          secureTextEntry={!showPassword}
+          onChangeText={(nv) => {
+            updateData(nv, 'password');
+          }}
+        />
+        <Icon
+          name={showPassword ? 'visibility' : 'visibility-off'}
+          size={24}
+          color='grey'
+          onPress={() => setShowPassword(!showPassword)}
+        />
+      </View>
 
-    return (
-        <View style={styles.container}>
-            <Text style={styles.title}>LOGIN</Text>
-            <TextInput
-                style={[styles.input]}
-                placeholder="Ingresa tu correo"
-                placeholderTextColor='#fff'
-                onChangeText={nv => { updateData(nv, 'email') }}
-            />
-            <TextInput
-                style={styles.input}
-                placeholder="Ingresa tu contrasena"
-                placeholderTextColor='#fff'
-                secureTextEntry
-                onChangeText={nv => { updateData(nv, 'password') }}
-            />
-            <Button title="INICIAR SESION" onPress={singin} />
-            <View style={styles.cambio}>
-                <Text style={styles.texto}>
-                    ¿No tienes una cuenta?
-                </Text>
-                <Button title="REGISTRARSE" onPress={changeForm} />
-            </View>
-        </View>
-    )
+      <TouchableOpacity style={styles.button} onPress={singin}>
+        <Text style={styles.buttonText}>INICIAR SESION</Text>
+      </TouchableOpacity>
+
+      <Text style={styles.texto}>¿No tienes una cuenta?</Text>
+      <TouchableOpacity style={styles.button} onPress={changeForm}>
+        <Text style={styles.buttonText}>REGISTRARSE</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
 
-
 const styles = StyleSheet.create({
-    container: {
-        width: '100%',
-        backgroundColor: '#000',
-        gap: 10,
-        alignItems: 'center'
-    },
-    title: {
-        color: 'white',
-        fontSize: 35,
-        fontWeight: 'bold'
-    },
-    btnText: {
-        fontSize: 20,
-    },
-    input: {
-        height: 50,
-        width: '80%',
-        backgroundColor: '#1e3040',
-        color: '#fff',
-        borderRadius: 20,
-        paddingHorizontal: 10,
-        borderWidth: 1.5,
-    },
-    texto: {
-        height: 50,
-        width: '80%',
-        color: '#fff',
-        textAlign: 'center', // Centra el texto horizontalmente
-    },
-    cambio: {
-        marginTop: 250,
-        position: 'absolute',
-        alignItems: 'center', 
-        justifyContent: 'center', 
-    },
-})
-
+  container: {
+    width: '100%',
+    backgroundColor: '#000',
+    gap: 10,
+    alignItems: 'center',
+  },
+  title: {
+    color: 'white',
+    fontSize: 35,
+    fontWeight: 'bold',
+  },
+  input: {
+    width: '90%',
+    backgroundColor: '#1e3040',
+    color: '#fff',
+    borderRadius: 20,
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+    borderWidth: 1.5,
+    fontSize: 18,
+    marginVertical: 8,
+  },
+  inputField: {
+    flex: 1,
+    color: '#fff',
+    fontSize: 18,
+  },
+  inputContainer: {
+    width: '90%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#1e3040',
+    borderRadius: 20,
+    paddingHorizontal: 15,
+    borderWidth: 1.5,
+    marginVertical: 10,
+    paddingVertical: 5,
+  },
+  texto: {
+    width: '80%',
+    color: '#fff',
+    textAlign: 'center',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  button: {
+    backgroundColor: '#00509e',
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+    borderRadius: 20,
+    alignItems: 'center',
+    marginVertical: 10,
+    width: '60%',
+    alignSelf: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
--- src/Components/RegisterForm.jsx
+++ src/Components/RegisterForm.jsx
@@ -1,237 +1,342 @@
-import { Text, Button, View, StyleSheet, TextInput } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
 import { Picker } from '@react-native-picker/picker';
-import { useState } from "react";
-
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-import { app, db } from "./../utils/firebase.js";
+import { useState } from 'react';
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+import { app, db } from './../utils/firebase.js';
 import { collection, addDoc } from 'firebase/firestore';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { validateEmail } from '../controller';
 
+export default function RegisterForm({
+  changeForm = () => {},
+  setRol,
+  setUsuario,
+}) {
+  const [rolI, setRolI] = useState('');
+  const [sexo, setSexo] = useState('');
+  const [data, setData] = useState({
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+  const [datosCompletos, setDatosCompletos] = useState({
+    Nombre: '',
+    Apellido_Paterno: '',
+    Apellido_Materno: '',
+    Contraseña: '',
+    Correo: '',
+    rol: '',
+    sexo: '',
+  });
 
+  const [error, setError] = useState({
+    emailE: '',
+    passwordE: '',
+    confirmPasswordE: '',
+  });
 
-import { validateEmail } from "../controller";
+  const updateData = (newValue, campo) => {
+    const newData = { ...data };
+    newData[campo] = newValue;
+    setData(newData);
+  };
 
-export default function RegisterForm(
-    {
-        changeForm = () => { }, setRol, setUsuario
+  const updateDatosCom = (newValue, campo) => {
+    const newData = { ...datosCompletos };
+    newData[campo] = newValue;
+    setDatosCompletos(newData);
+  };
+
+  const validateData = (data) => {
+    const { email, password, confirmPassword } = data;
+    let newError = { emailE: '', passwordE: '', confirmPasswordE: '' };
+
+    // Validación del correo
+    if (!email) {
+      newError.emailE = 'El email es obligatorio';
+    } else if (!validateEmail(email)) {
+      newError.emailE = 'El email no es válido';
     }
-) {
-    const [rolI, setRolI] = useState('')
-    const [sexo, setSexo] = useState('');
-    const [data, setData] = useState({ email: '', password: '', confirmPassword: '' })
-    const [datosCompletos, setDatosCompletos] = useState({ Nombre: '', Apellido_Paterno: '', Apellido_Materno: '', Contraseña: '', Correo: '', rol: '', sexo: '' })
 
-    const [error, setError] = useState({ emailE: '', passwordE: '', confirmPasswordE: '' })
-
-
-    const updateData = (newValue, campo) => {
-        const newData = { ...data }
-        newData[campo] = newValue
-        setData(newData)
+    // Validación de la contraseña
+    if (!password) {
+      newError.passwordE = 'El password es obligatorio';
+    } else if (password.length <= 6) {
+      newError.passwordE = 'El password debe ser mayor a 6 caracteres';
     }
 
-    const updateDatosCom = (newValue, campo) => {
-        const newData = { ...datosCompletos }
-        newData[campo] = newValue
-        setDatosCompletos(newData)
+    if (!confirmPassword) {
+      newError.confirmPasswordE = 'Debes confirmar el password';
+    } else if (confirmPassword.length <= 6) {
+      newError.confirmPasswordE =
+        'La confirmación debe ser mayor a 6 caracteres';
+    } else if (password !== confirmPassword) {
+      newError.confirmPasswordE = 'Las contraseñas no coinciden';
     }
 
-    const validateData = (data) => {
-        const { email, password, confirmPassword } = data;
-        let newError = { emailE: '', passwordE: '', confirmPasswordE: '' };
-    
-        // Validación del correo
-        if (!email) {
-            newError.emailE = 'El email es obligatorio';
-        } else if (!validateEmail(email)) {
-            newError.emailE = 'El email no es válido';
-        }
-    
-        // Validación de la contraseña
-        if (!password) {
-            newError.passwordE = 'El password es obligatorio';
-        } else if (password.length <= 6) {
-            newError.passwordE = 'El password debe ser mayor a 6 caracteres';
-        }
-    
-        if (!confirmPassword) {
-            newError.confirmPasswordE = 'Debes confirmar el password';
-        } else if (confirmPassword.length <= 6) {
-            newError.confirmPasswordE = 'La confirmación debe ser mayor a 6 caracteres';
-        } else if (password !== confirmPassword) {
-            newError.confirmPasswordE = 'Las contraseñas no coinciden';
-        }
-    
-        return newError;
-    };
-    
+    return newError;
+  };
 
-    const registerUser = async (email, password, datosCompletos) => {
-        const auth = getAuth(app);
-        try {
-            const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-            const user = userCredential.user;
-            const userCollection = collection(db, "usuarios");
-            const docRef = await addDoc(userCollection, {
-                uid: user.uid,
-                Nombre: datosCompletos.Nombre,
-                Apellido_Paterno: datosCompletos.Apellido_Paterno,
-                Apellido_Materno: datosCompletos.Apellido_Materno,
-                rol: datosCompletos.rol,
-                sexo: datosCompletos.sexo,
-                Correo: email,
-                Contraseña: password
-            });
-            setRol(datosCompletos.rol);
-            setUsuario({
-                "id": docRef.id,
-                "Nombre": datosCompletos.Nombre,
-                "ApellidoP": datosCompletos.Apellido_Paterno,
-                "ApellidoM": datosCompletos.Apellido_Materno
-            });
-        } catch (error) {
-            //console.error("Error al guardar el empleado:", error);
-            if (error.code === "auth/email-already-in-use") {
-                alert("Este correo ya está registrado. Por favor, utiliza otro.");
-            } else {
-                alert("Error al guardar los datos. Intenta nuevamente.");
-            }
-        }
-    };
-    
+  const registerUser = async (email, password, datosCompletos) => {
+    const auth = getAuth(app);
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      const user = userCredential.user;
+      const userCollection = collection(db, 'usuarios');
+      const docRef = await addDoc(userCollection, {
+        uid: user.uid,
+        Nombre: datosCompletos.Nombre,
+        Apellido_Paterno: datosCompletos.Apellido_Paterno,
+        Apellido_Materno: datosCompletos.Apellido_Materno,
+        rol: datosCompletos.rol,
+        sexo: datosCompletos.sexo,
+        Correo: email,
+        Contraseña: password,
+      });
+      setRol(datosCompletos.rol);
+      setUsuario({
+        id: docRef.id,
+        Nombre: datosCompletos.Nombre,
+        ApellidoP: datosCompletos.Apellido_Paterno,
+        ApellidoM: datosCompletos.Apellido_Materno,
+      });
+    } catch (error) {
+      //console.error("Error al guardar el empleado:", error);
+      if (error.code === 'auth/email-already-in-use') {
+        alert('Este correo ya está registrado. Por favor, utiliza otro.');
+      } else {
+        alert('Error al guardar los datos. Intenta nuevamente.');
+      }
+    }
+  };
 
-    const check = async () => {
-        const { email, password, confirmPassword } = data;
-        const newError = validateData(data);
-        setError(newError);
+  const check = async () => {
+    const { email, password, confirmPassword } = data;
+    const newError = validateData(data);
+    setError(newError);
 
-        if (!datosCompletos.Nombre || !datosCompletos.Apellido_Paterno || !datosCompletos.Apellido_Materno || !datosCompletos.rol || !datosCompletos.sexo) {
-            alert("Por favor, completa todos los campos.");
-            return;
-        }
+    if (!datosCompletos.Nombre) {
+      alert('Por favor, llene su nombre.');
+      return;
+    }
 
-        if (!newError.emailE && !newError.passwordE && !newError.confirmPasswordE) {
-            console.log('---------- No hay errores -----------');
-            await registerUser(email, password, datosCompletos);
-        } else {
-            if (newError.emailE) alert(newError.emailE);
-            if (newError.passwordE) alert(newError.passwordE);
-            if (newError.confirmPasswordE) alert(newError.confirmPasswordE);
-        }
-    };
-    return (
-        <View style={styles.container}>
-            <TextInput
-                style={styles.input}
-                placeholder="Nombre"
-                placeholderTextColor='#fff'
-                onChangeText={nv => { updateDatosCom(nv, 'Nombre') }}
-            />
-            <TextInput
-                style={styles.input}
-                placeholder="Apellido Paterno"
-                placeholderTextColor='#fff'
-                onChangeText={nv => { updateDatosCom(nv, 'Apellido_Paterno') }}
-            />
-            <TextInput
-                style={styles.input}
-                placeholder="Apellido Materno"
-                placeholderTextColor='#fff'
-                onChangeText={nv => { updateDatosCom(nv, 'Apellido_Materno') }}
-            />
-            <View style={styles.selectContainer}>
-                <Picker
-                    selectedValue={rolI}
-                    style={styles.picker}
-                    onValueChange={nv => { updateDatosCom(nv, 'rol') }}
-                >
-                    <Picker.Item label=" Rol" value="" />
-                    <Picker.Item label="Docente" value="Docente" />
-                    <Picker.Item label="Estudiante" value="Estudiante" />
-                </Picker>
-                <Picker
-                    selectedValue={sexo}
-                    style={styles.picker}
-                    onValueChange={nv => { updateDatosCom(nv, 'sexo') }}
-                >
-                    <Picker.Item label="sexo" value="" />
-                    <Picker.Item label="Masculino" value="Masculino" />
-                    <Picker.Item label="Femenino" value="Femenino" />
-                </Picker>
-            </View>
-            <TextInput
-                style={styles.input}
-                placeholder="Ingresa tu correo"
-                placeholderTextColor='#fff'
-                onChangeText={nv => { updateData(nv, 'email') }}
-            />
-            <TextInput
-                style={styles.input}
-                placeholder="Ingresa tu contrasena"
-                placeholderTextColor='#fff'
-                secureTextEntry
-                onChangeText={nv => { updateData(nv, 'password') }}
-            />
-            <TextInput
-                style={styles.input}
-                placeholder="Confirma tu contrasena"
-                placeholderTextColor='#fff'
-                secureTextEntry
-                onChangeText={nv => { updateData(nv, 'confirmPassword') }}
-            />
+    if (!datosCompletos.Apellido_Paterno) {
+      alert('Por favor, llene su apellido paterno.');
+      return;
+    }
 
-            <Button
-                title="Registrate"
-                onPress={check}
-            />
-            <View style={styles.cambio}>
-                <Button
-                    title="Iniciar Sesion"
-                    onPress={changeForm}
-                    style={styles.btnText}
-                />
-            </View>
+    if (!datosCompletos.Apellido_Materno) {
+      alert('Por favor, llene su apellido materno.');
+      return;
+    }
+
+    if (!datosCompletos.rol) {
+      alert('Por favor, seleccione su rol.');
+      return;
+    }
+
+    if (!datosCompletos.sexo) {
+      alert('Por favor, seleccione su sexo.');
+      return;
+    }
+
+    if (!newError.emailE && !newError.passwordE && !newError.confirmPasswordE) {
+      console.log('---------- No hay errores -----------');
+      await registerUser(email, password, datosCompletos);
+    } else if (newError.emailE) {
+      alert(newError.emailE);
+    } else if (newError.passwordE) {
+      alert(newError.passwordE);
+    } else if (newError.confirmPasswordE) {
+      alert(newError.confirmPasswordE);
+    }
+  };
+
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder='Nombre'
+        placeholderTextColor='#fff'
+        onChangeText={(nv) => {
+          updateDatosCom(nv, 'Nombre');
+        }}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder='Apellido Paterno'
+        placeholderTextColor='#fff'
+        onChangeText={(nv) => {
+          updateDatosCom(nv, 'Apellido_Paterno');
+        }}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder='Apellido Materno'
+        placeholderTextColor='#fff'
+        onChangeText={(nv) => {
+          updateDatosCom(nv, 'Apellido_Materno');
+        }}
+      />
+      <View style={styles.selectContainer}>
+        <View style={styles.pickerContainer}>
+          <Picker
+            selectedValue={rolI}
+            style={styles.picker}
+            onValueChange={(nv) => {
+              updateDatosCom(nv, 'rol');
+            }}
+          >
+            <Picker.Item label=' Rol' value='' />
+            <Picker.Item label='Docente' value='Docente' />
+            <Picker.Item label='Estudiante' value='Estudiante' />
+            <Picker.Item label='Administrador' value='Administrador' />
+          </Picker>
         </View>
-    );
-};
+        <View style={styles.pickerContainer}>
+          <Picker
+            selectedValue={sexo}
+            style={styles.picker}
+            onValueChange={(nv) => {
+              updateDatosCom(nv, 'sexo');
+            }}
+          >
+            <Picker.Item label='sexo' value='' />
+            <Picker.Item label='Masculino' value='Masculino' />
+            <Picker.Item label='Femenino' value='Femenino' />
+          </Picker>
+        </View>
+      </View>
+      <TextInput
+        style={styles.input}
+        placeholder='Ingresa tu correo'
+        placeholderTextColor='#fff'
+        onChangeText={(nv) => {
+          updateData(nv, 'email');
+        }}
+      />
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.inputField}
+          placeholder='Ingresa tu contraseña'
+          placeholderTextColor='#fff'
+          secureTextEntry={!showPassword}
+          onChangeText={(nv) => {
+            updateData(nv, 'password');
+          }}
+        />
+        <Icon
+          name={showPassword ? 'visibility' : 'visibility-off'}
+          size={24}
+          color='grey'
+          onPress={() => setShowPassword(!showPassword)}
+        />
+      </View>
+
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.inputField}
+          placeholder='Confirma tu contraseña'
+          placeholderTextColor='#fff'
+          secureTextEntry={!showConfirmPassword}
+          onChangeText={(nv) => {
+            updateData(nv, 'confirmPassword');
+          }}
+        />
+        <Icon
+          name={showConfirmPassword ? 'visibility' : 'visibility-off'}
+          size={24}
+          color='grey'
+          onPress={() => setShowConfirmPassword(!showConfirmPassword)}
+        />
+      </View>
+
+      <TouchableOpacity style={styles.button} onPress={check}>
+        <Text style={styles.buttonText}>Registrate</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.button} onPress={changeForm}>
+        <Text style={styles.buttonText}>Iniciar Sesion</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
-    container: {
-        width: '90%',
-        maxHeight: '80%',  // Limita la altura al 80% de la pantalla
-        gap: 10,
-        alignItems: 'center',
-    },
-    btnText: {
-        fontSize: 20,
-        borderRadius: 20
-    },
-    input: {
-        height: 40,  // Ajusta el tamaño del input a un tamaño más pequeño
-        width: '80%',
-        backgroundColor: '#1e3040',
-        color: '#fff',
-        borderRadius: 20,
-        paddingHorizontal: 10,
-        borderWidth: 1.5,
-    },
-    cambio: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginBottom: 20,  // Un pequeño margen en la parte inferior
-    },
-    selectContainer: {
-        width: '90%',
-        borderWidth: 1.5,
-        flexDirection: 'row', // Los coloca en la misma línea
-        justifyContent: 'space-between', // Asegura que se distribuyan correctamente
-    },
-    picker: {
-        backgroundColor: '#1e3040',
-        borderRadius: 40,
-        paddingHorizontal: 10,
-        height: 50,
-        width: '48%',
-        fontSize: 1, // Tamaño de la letra más pequeño
-        color: '#fff', // Color blanco para el texto
-    },
+  container: {
+    width: '90%',
+    maxHeight: '80%', // Limita la altura al 80% de la pantalla
+    alignItems: 'flex-start',
+  },
+  input: {
+    width: '100%',
+    backgroundColor: '#1e3040',
+    color: '#fff',
+    borderRadius: 20,
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+    borderWidth: 1.5,
+    fontSize: 18,
+    marginVertical: 8,
+  },
+  selectContainer: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginVertical: 10,
+  },
+  pickerContainer: {
+    backgroundColor: '#1e3040',
+    borderRadius: 25,
+    width: '48%',
+    paddingHorizontal: 5,
+    borderWidth: 1.5,
+    borderColor: '#1e3040',
+  },
+  picker: {
+    color: '#fff',
+  },
+  inputField: {
+    flex: 1,
+    color: '#fff',
+    fontSize: 18,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#1e3040',
+    borderRadius: 20,
+    paddingHorizontal: 15,
+    borderWidth: 1.5,
+    marginVertical: 10,
+    paddingVertical: 5,
+  },
+  button: {
+    backgroundColor: '#00509e',
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+    borderRadius: 20,
+    alignItems: 'center',
+    marginVertical: 10,
+    width: '70%',
+    alignSelf: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
 });
